774b1549fe3c4ddfd6ba329710d3a775c9eef32d
Bri Pickstone <bripickstone@gmail.com>
2023-06-21 17:16:17 +0100

    updated calculations

--- Planet_Spec_Indices.R
+++ Planet_Spec_Indices.R
@@ -4,16 +4,26 @@ library(colorspace)
 library(tidyverse)
 library(sf)
 library(raster)
-data_path <- "/Users/bri/Library/CloudStorage/OneDrive-UniversityofExeter/University/Dissertation/Data"
 
+
+# Import Planet Labs Data -------------------------------------------------
+
+#data_path <- "/Users/bri/Library/CloudStorage/OneDrive-UniversityofExeter/University/Dissertation/Data"
+data_path <- "/raid/home/bp424/Documents/MTHM603/Data"
 kat_planet <- rast(file.path(data_path,"Katingan-Comp-22-median.tif"))
 kat_planet
 
 print(names(kat_planet))
-kat_planet
 
 
 
+# import the LiDAR data ---------------------------------------------------
+
+# Read LiDAR dtm and dsm file 
+dtm <- rast(file.path(data_path,"Original-DEMS/katingan_DEMS/katingan_DTM.tif"))
+dsm <- rast(file.path(data_path,"Original-DEMS/katingan_DEMS/katingan_DSM.tif"))
+
+#scale down the 8 existing spectral bands
 kat_planet$coastal_blue <- (kat_planet$coastal_blue/10000)
 kat_planet$blue <- (kat_planet$blue/10000)
 kat_planet$green_i <- (kat_planet$green_i/10000)
@@ -22,7 +32,6 @@ kat_planet$yellow <- (kat_planet$yellow/10000)
 kat_planet$red <- (kat_planet$red/10000)
 kat_planet$rededge <- (kat_planet$rededge/10000)
 kat_planet$nir <- (kat_planet$nir/10000)
-kat_planet
 
 
 # Do the band math - HG examples
@@ -31,7 +40,7 @@ kat_planet$NDRE <- (kat_planet$nir - kat_planet$rededge)/(kat_planet$nir +kat_pl
 kat_planet$EVI <- 2.5 * ((kat_planet$nir) - (kat_planet$red)) /
   ((kat_planet$nir) + 6 * (kat_planet$red) - 7.5 * (kat_planet$blue) + 1)
 
-
+#Calculations by BP
 kat_planet$AVI <- (kat_planet$nir * (1.0 - kat_planet$red) * 
                      (kat_planet$nir - kat_planet$red))/3
 kat_planet$RDVI <- (kat_planet$nir - kat_planet$red)/((kat_planet$nir + kat_planet$red)^0.5)
@@ -41,6 +50,21 @@ kat_planet$GNDVI <- (kat_planet$nir - kat_planet$green)/
 kat_planet$RGBVI <- (kat_planet$green^2 - kat_planet$blue * kat_planet$red)/
   (kat_planet$green^2 + kat_planet$blue * kat_planet$red)
 kat_planet$BNDVI <- (kat_planet$nir - kat_planet$blue)/(kat_planet$nir + kat_planet$blue)
+kat_planet$CVI <- (kat_planet$nir * kat_planet$red)/(kat_planet$green^2.0)
+kat_planet$GBNDVI <- (kat_planet$nir - (kat_planet$green + kat_planet$blue))/
+  (kat_planet$nir + (kat_planet$green + kat_planet$blue))
+kat_planet$gli <- (2.0 * kat_planet$green - kat_planet$red - kat_planet$blue)/
+  (2.0 * kat_planet$green + kat_planet$red + kat_planet$blue)
+
+#kat_planet$GEMI <- ((2 *((kat_planet$nir^2.0)-(kat_planet$red^2.0)) + 1.5 * 
+#                       kat_planet$nir + 0.5 * kat_planet$red)/kat_planet$nir 
+#                    + kat_planet$red + 0.5)) * (1.0 - 0.25 * ((2.0 *((kat_planet$nir^2.0)- 
+#                                                                       (kat_planet$red^2))))
+
+
+# Calculate Canopy Height Model  ------------------------------------------
+kat_planet$CHM <- dsm - dtm
+canopy_height
 
 
 #plot the first 8 bands 
