d7ee6517d0304e1b4ac45eed306f5111520e0114
Bri Pickstone <bripickstone@gmail.com>
2023-06-28 13:53:18 +0100

    updates to MLR

--- MLR.R
+++ MLR.R
@@ -6,10 +6,7 @@ library(terra)
 library(tidyverse)
 library(dplyr)
 library(ggpmisc)
-library(cluster)
-install.packages("factoextra")
-library(factoextra)
-
+library(lightgbm)
 
 
 # set seed ----------------------------------------------------------------
@@ -22,8 +19,22 @@ kat_planet <- rast(file.path(data_path,"Katingan-Comp-22-median.tif"))
 df <- read_csv(file.path(data_path, "df.csv"))
 
 
-# generate a task ---------------------------------------------------------
+# find optimal number of clusters -----------------------------------------
+# Exclude x and y from the dataframe
+data <- df[, !(colnames(df) %in% c("x", "y"))]
 
+# Define the range of cluster numbers to evaluate
+k_values <- 2:7
+
+# Perform k-means clustering for each k value and calculate total within-cluster sum of squares (wss)
+wss <- sapply(k_values, function(k) {
+  kmeans(data, centers = k)$tot.withinss
+})
+
+# Plot the elbow curve
+plot(k_values, wss, type = "b", pch = 19, frame = FALSE, xlab = "Number of Clusters (k)", ylab = "Total Within-Cluster Sum of Squares (wss)")
+
+# generate a task ---------------------------------------------------------
 task = mlr3spatiotempcv::TaskRegrST$new(
   id = "kat_base_CHM", 
   backend= df,
@@ -34,44 +45,57 @@ task = mlr3spatiotempcv::TaskRegrST$new(
     crs= terra::crs(kat_planet))
 )
 
-# find optimal number of clusters -----------------------------------------
-# Exclude x and y from the dataframe
-data <- df[, !(colnames(df) %in% c("x", "y"))]
-
-# Define the range of cluster numbers to evaluate
-k_values <- 2:10
-
-# Perform k-means clustering for each k value and calculate total within-cluster sum of squares (wss)
-wss <- sapply(k_values, function(k) {
-  kmeans(data, centers = k)$tot.withinss
-})
-
-# Plot the elbow curve
-plot(k_values, wss, type = "b", pch = 19, frame = FALSE, xlab = "Number of Clusters (k)", ylab = "Total Within-Cluster Sum of Squares (wss)")
-
-# Identify the optimal number of clusters based on the elbow point
-elbow_point <- fviz_nbclust(data, kmeans, method = "wss")$ch
-
-# Add a vertical line at the elbow point
-abline(v = elbow_point, lty = 2)
-
-
 # define a resampling plan ------------------------------------------------
-
-spcv_plan <- mlr3::rsmp("repeated_spcv_coords", folds = 6, repeats = 1)
-autoplot(spcv_plan, task = task, fold_id = 1:3)
+spcv_plan <- mlr3::rsmp("repeated_spcv_coords", folds = 4, repeats = 5)
+#autoplot(spcv_plan, task = task, fold_id = 1:3)
 
 
 # define some learners ----------------------------------------------------
 
-filt.lrn <- mlr3::lrn("regr.lightgbm", predict_type = "response", boosting ="dart")
+filt.lrn <- mlr3::lrn("regr.lightgbm")
 
-.lrn <- mlr3::lrn("regr.lightgbm", predict_type = "response", boosting = "dart", 
+.lrn <- mlr3::lrn("regr.lightgbm", predict_type = "response", boosting = "gbdt", 
                   learning_rate = to_tune(lower = 0.01, upper = 0.1, logscale = TRUE), 
-                  max_bin= to_tune(lower = 100, upper= 1000), 
-                  num_leaves = to_tune(lower = 10, upper = 75))
+                  max_bin= to_tune(lower = 100, upper= 1500), 
+                  num_leaves = to_tune(lower = 10, upper = 100))
 
 
 # set up a pipeline learner -----------------------------------------------
+.lrn.filt <- 
+  po("subsample", frac = 0.6) %>>%
+  po("filter", filter = flt("importance", learner= filt.lrn), filter.frac=to_tune(0.1, 0.3))%>>%
+  mlr3::lrn("regr.lm")  #simple linear model
+  #.lrn
 
+plot(.lrn.filt)
+
+combined.lrnr <- as_learner(.lrn.filt)
+
+
+# tune the model ----------------------------------------------------------
+
+at <- auto_tuner(
+  method = tnr("random_search"),
+  learner = combined.lrnr,
+  resampling = spcv_plan,
+  measure = mlr3::msr(c("regr.rsq")),
+  term_evals = 10
+)
+
+future::plan("multisession", workers = 10)
+progressr::with_progress(expr= {
+  at$train(task)
+})
+
+
+# do spatial resampling ---------------------------------------------------
+resample_lgbm <- progressor::with_progressor(expr {
+  mlr3::resample(
+    task = task, 
+    learner = at$learner, 
+    resampling = spcv_plan,
+    store_models = FALSE, 
+    encapsulate = "evaluate"
+  )
+})
 
